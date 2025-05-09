269b034882055529b332cd6a3b59f12330d4428a
bim.krismon <bim.krismon@permatabank.co.id>
2025-05-09 15:12:54 +0700

    dev[typescript][data_array] - declare variable data type array

---
+++ dist/test/data-array.test.js
@@ -0,0 +1,10 @@
+"use strict";
+describe('Data array', function () {
+    it('Should data array', function () {
+        const names = ['Bimbim Mulya Krismon', 'JL. Verina X Graha Raya Blok J No 28'];
+        const noUrut = [1, 2, 3];
+        console.info(names);
+        console.info(noUrut);
+        names[0] = 1;
+    });
+});
---
+++ test/data-array.test.ts
@@ -0,0 +1,10 @@
+describe('Data array', function() {
+  it('Should data array', function() {
+    const names : string[] = ['Bimbim Mulya Krismon', 'JL. Verina X Graha Raya Blok J No 28'];
+    const noUrut : number[] = [1, 2, 3];
+
+    console.info(names);
+    console.info(noUrut);
+
+  })
+})
