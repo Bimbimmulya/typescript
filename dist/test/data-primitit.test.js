320456d36dd49358356c6fa8d4636eca50b36a48
bim.krismon <bim.krismon@permatabank.co.id>
2025-05-09 14:33:24 +0700

    dev[typescript][data_primitif] - declare variable with type data

---
+++ dist/test/data-primitit.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Declare type data', function () {
+    it('Should must declare', function () {
+        const name = 'Bimbim Mulya Krismon';
+        const age = 24;
+        const isVIP = true;
+        console.info(name);
+        console.info(age);
+        console.info(isVIP);
+    });
+});
---
+++ test/data-primitit.test.ts
@@ -0,0 +1,11 @@
+describe('Declare type data', function () {
+  it('Should must declare', function() {
+    const name : String = 'Bimbim Mulya Krismon';
+    const age : Number = 24;
+    const isVIP : Boolean = true
+    
+    console.info(name);
+    console.info(age);
+    console.info(isVIP);
+  })
+})
